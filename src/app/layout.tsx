42449cc467c0e49484cfb5723a2325f0ee9f0935
Thiago Roberto Pedro <thiagorpedro@gmail.com>
2025-11-30 20:49:23 +0000

    Faça a página dashboard ser a página index

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,33 @@
-import type {Metadata} from 'next';
+import type { Metadata, Viewport } from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
+import type { ReactNode } from 'react';
+import { SidebarProvider, Sidebar, SidebarInset } from '@/components/ui/sidebar';
+import { DashboardNav } from '@/components/dashboard-nav';
+import { Header } from '@/components/header';
+import { cookies } from 'next/headers';
+
 
 export const metadata: Metadata = {
   title: 'Krav Magá Ipiranga',
   description: 'Gerencie sua academia de Krav Magá com facilidade.',
 };
 
+export const viewport: Viewport = {
+  themeColor: 'hsl(var(--background))',
+  width: 'device-width',
+  initialScale: 1,
+}
+
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const layout = cookies().get("sidebar_state");
+  const defaultOpen = layout ? layout.value === "true" : true;
+
   return (
     <html lang="pt-BR" suppressHydrationWarning>
       <head>
@@ -20,7 +36,17 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
-        {children}
+        <SidebarProvider defaultOpen={defaultOpen}>
+          <Sidebar>
+            <DashboardNav />
+          </Sidebar>
+          <SidebarInset>
+            <Header />
+            <main className="p-4 sm:p-6 lg:p-8 bg-secondary/50 min-h-[calc(100vh-4rem)]">
+              {children}
+            </main>
+          </SidebarInset>
+        </SidebarProvider>
         <Toaster />
       </body>
     </html>
